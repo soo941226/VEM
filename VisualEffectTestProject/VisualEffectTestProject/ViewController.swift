50f33f992f98a11b949e2ec181717fe9eba365c5
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-12-20 22:15:58 +0900

    feat: Added test project

---
+++ VisualEffectTestProject/VisualEffectTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisualEffectTestProject
+//
+//  Created by kjs on 2021/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
