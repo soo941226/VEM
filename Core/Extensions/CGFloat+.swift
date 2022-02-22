45785610acc85cb2640781056898685f7d7ffabb
soo941226 <83933153+soo941226@users.noreply.github.com>
2022-02-22 23:34:20 +0900

    refactor: subtracted a extension about CGFloat
    * renamed a file name

---
+++ Core/Extensions/CGFloat+.swift
@@ -0,0 +1,18 @@
+//
+//  CGFloat+.swift
+//  VEMTestProject
+//
+//  Created by kjs on 2022/02/22.
+//
+
+import CoreGraphics
+
+extension CGFloat {
+    static var circumference: CGFloat {
+        return .pi * 2
+    }
+
+    static var quarterArc: CGFloat {
+        return .pi / 2
+    }
+}
---
+++ Core/Extensions/UIImage+.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  VEMTestProject
+//
+//  Created by kjs on 2021/12/27.
+//
+
+#if os(iOS)
+import UIKit
+
+extension UIImage {
+    func filled(with color: UIColor, isOpaque: Bool = false) -> UIImage? {
+        let format = imageRendererFormat
+        format.opaque = isOpaque
+        
+        return UIGraphicsImageRenderer(size: size, format: format).image { _ in
+            color.set()
+            withRenderingMode(.alwaysTemplate).draw(at: .zero)
+        }
+    }
+}
+#endif
